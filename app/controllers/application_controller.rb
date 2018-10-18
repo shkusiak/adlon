1958cb99bd4de93d014f8c59b8b12ade79e552c8
github username <github email address>
2018-10-18 19:38:25 -0400

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,33 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect to '/login'
+      end
+    end
+
+  end
+end
